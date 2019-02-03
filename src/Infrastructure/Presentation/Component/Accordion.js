e41c9c03ec5d495377aa69fe74819efcbda44789
Ivan Rubio <ivan@suntransfers.com>
2019-02-03 19:14:15 +0100

    UI layer for Accordion

---
+++ src/Infrastructure/Presentation/Component/Accordion.js
@@ -0,0 +1,67 @@
+import '../Common/styles/base.scss'
+import './_accordion.scss';
+
+export default class Accordion {
+    constructor(el) {
+        const container =
+            typeof el === 'string' ? document.querySelector(el) : el;
+
+        if (container === null) {
+            throw Error(
+                'Invalid argument: The object passed cannot be found or is not considered a valid DOM item'
+            );
+        }
+
+        /**
+         * Default options
+         */
+        this.settings = {
+            id: `irh-accordion-${Math.ceil(Math.random() * 10000) + 1}`,
+            suitComponent: {
+                main: 'irh-Accordion',
+                header: 'irh-Accordion-header',
+                panel: 'irh-Accordion-panel',
+                isExpanded: 'is-expanded',
+                isLoading: 'is-loading'
+            }
+        };
+
+        this.selectedItem = null;
+
+        this.container = container;
+        this.container.classList.add(this.settings.suitComponent.main);
+        this.container.setAttribute('id', this.settings.id);
+        this.container.addEventListener('click', this.handleClick);
+
+        const headers = Array.from(this.container.querySelectorAll('dt'));
+        this.applySUIT(headers);
+    }
+
+    applySUIT(headers) {
+        headers.map(header => {
+            header.classList.add(this.settings.suitComponent.header);
+            header.nextElementSibling.classList.add(this.settings.suitComponent.panel);
+        });
+    }
+
+    handleClick = event => {
+        const {header, isExpanded} = this.settings.suitComponent;
+        const headerInstance = event.target;
+
+        if (!event.target.classList.contains(header)) {
+            return;
+        }
+
+        if (this.selectedItem) {
+            this.selectedItem.classList.toggle(isExpanded);
+        }
+
+        if (this.selectedItem === headerInstance) {
+            this.selectedItem = null;
+            return;
+        }
+
+        headerInstance.classList.add(isExpanded);
+        this.selectedItem = headerInstance;
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import Accordion from './Infrastructure/Presentation/Component/Accordion';
+
+const definitionList = document.querySelector('dl');
+new Accordion(definitionList);
